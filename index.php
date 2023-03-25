2e7e67a70649ccbc2a7b34e03ba7dee0a6deb9a9
olaormundos <vanderleidasilvaormundo@gmail.com>
2023-03-25 15:17:16 -0300

    frist commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Receitas
+Description: Esse plugin foi desenvolvido com o intuito de o usuário adicionar e configurar receitas em seu blog profissional, dando a possibilidade de criar um sistema complexo com várias personalizações. 
+Version: 1.0
+Author: Olá Ormundos
+Author URI: https://github.com/olaormundos/
+Requires at least: 5.0
+Tested up to: 5.4
+Requires PHP: 7.4
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
