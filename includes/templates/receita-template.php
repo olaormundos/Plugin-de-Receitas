d9b08086d6384bad688a016079f12153b4c304b5
olaormundos <vanderleidasilvaormundo@gmail.com>
2023-03-29 23:51:59 -0300

    concatenando dados

---
+++ includes/templates/receita-template.php
@@ -0,0 +1,7 @@
+<ul>
+  <li>Ingredientes: INGREDIENTES_PH</li>
+  <li>Tempo: TEMPO_PH</li>
+  <li>Utensilios: UTENSILIOS_PH</li>
+  <li>Dificuldade: DIFICULDADE_PH</li>
+  <li>Tipo: TIPO_PH</li>
+</ul>
